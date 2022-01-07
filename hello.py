c3878a9a84a10eb8b0af781d4b7a39216cb47954
Andi Meier <andi@daemeier-clan.ch>
2022-01-07 13:11:02 +0100

    Configure logging

--- hello.py
+++ hello.py
@@ -1,6 +1,25 @@
 from flask import Flask
 from flask_cors import CORS
+from logging.config import dictConfig
 import requests
+
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://flask.logging.wsgi_errors_stream',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'DEBUG',
+        'handlers': ['wsgi']
+    }
+})
+
 
 app = Flask(__name__)
 cors = CORS(app)
