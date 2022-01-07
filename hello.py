365b9a647361ad6fa43b0885de1cbc08077b8137
Andi Meier <andi@daemeier-clan.ch>
2022-01-07 13:11:46 +0100

    Use logging

--- hello.py
+++ hello.py
@@ -28,28 +28,34 @@ mystrom_switch_base_url = 'http://192.168.0.50/'
 
 @app.route('/')
 def index():
+    app.logger.info('index')
     return 'Index Page'
 
 @app.route('/hello')
 def hello():
+    app.logger.info('hello')
     return 'Hello, World'
 
 @app.route('/switch-status')
 def status():
+    app.logger.info('status...')
     response_content = requests.get(mystrom_switch_base_url + 'report').content
-    print('status: {}'.format(response_content))
+    app.logger.info('status: {}'.format(response_content))
     return response_content
 
 @app.route('/switch-on')
 def switch_on():
+    app.logger.info('switch on')
     return requests.get(mystrom_switch_base_url + 'relay?state=1').content
 
 @app.route('/switch-off')
 def switch_off():
+    app.logger.info('switch off')
     return requests.get(mystrom_switch_base_url + 'relay?state=0').content
 
 @app.route('/switch-toggle')
 def switch_toggle():
+    app.logger.info('switch toggle...')
     response_content = requests.get(mystrom_switch_base_url + 'toggle').content
-    print('switch-toggle: {}'.format(response_content))
+    app.logger.info('switch-toggle: {}'.format(response_content))
     return response_content
